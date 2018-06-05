5239e8ba7721f4c7f35edcfc2e222450f63ef367
hcarvajal <hcarvajal@DESKTOP-8SVJ6GP>
2018-06-05 16:34:11 -0400

    Add project files.

---
+++ LeagueExpress/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LeagueExpress.MvcApplication" Language="C#" %>
---
+++ LeagueExpress/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LeagueExpress.Startup))]
+namespace LeagueExpress
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
