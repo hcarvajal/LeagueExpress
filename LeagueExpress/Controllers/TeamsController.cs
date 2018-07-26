4b4a650255308a0005f23ca171f30531560dac54
Siraj Nasir <siraj_n@comcast.net>
2018-07-25 23:15:21 -0400

    Hello

--- LeagueExpress/Controllers/TeamsController.cs
+++ LeagueExpress/Controllers/TeamsController.cs
@@ -18,7 +18,7 @@ namespace LeagueExpress.Controllers
             return View();
         }
 
-        // load white page
+        // load White page
         public ActionResult White(String Team)
         {
             Team = "White";
@@ -29,14 +29,81 @@ namespace LeagueExpress.Controllers
                           return View(tPlayer);
         }
 
+        // load Purple page
+        public ActionResult Purple(String Team)
+        {
+            Team = "Purple";
+            var tPlayer = from p in _db.Players
+                          where p.currentTeam.ToUpper().Contains(Team)
+                          select p;
 
-       
+            return View(tPlayer);
+        }
 
+        // load Green page
+        public ActionResult Green(String Team)
+        {
+            Team = "Green";
+            var tPlayer = from p in _db.Players
+                          where p.currentTeam.ToUpper().Contains(Team)
+                          select p;
 
+            return View(tPlayer);
+        }
 
-       
-          
+        // load Yellow page
+        public ActionResult Yellow(String Team)
+        {
+            Team = "Yellow";
+            var tPlayer = from p in _db.Players
+                          where p.currentTeam.ToUpper().Contains(Team)
+                          select p;
 
+            return View(tPlayer);
+        }
 
+        // load Black page
+        public ActionResult Black(String Team)
+        {
+            Team = "Black";
+            var tPlayer = from p in _db.Players
+                          where p.currentTeam.ToUpper().Contains(Team)
+                          select p;
+
+            return View(tPlayer);
+        }
+
+        // load Red page
+        public ActionResult Red(String Team)
+        {
+            Team = "Red";
+            var tPlayer = from p in _db.Players
+                          where p.currentTeam.ToUpper().Contains(Team)
+                          select p;
+
+            return View(tPlayer);
+        }
+
+        // load Navy Blue page
+        public ActionResult NavyBlue(String Team)
+        {
+            Team = "Navy Blue";
+            var tPlayer = from p in _db.Players
+                          where p.currentTeam.ToUpper().Contains(Team)
+                          select p;
+
+            return View(tPlayer);
+        }
+
+        // load Grey page
+        public ActionResult Grey(String Team)
+        {
+            Team = "Grey";
+            var tPlayer = from p in _db.Players
+                          where p.currentTeam.ToUpper().Contains(Team)
+                          select p;
+
+            return View(tPlayer);
+        }
     }
 }
